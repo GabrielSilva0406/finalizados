90fc88330fa7376aa842d395205f2731734073a8
Gabriel Henrique da Silva <gabriel.henrique0406@outlook.com>
2025-03-18 22:42:34 -0300

    Update super-trunfo.c

--- super-trunfo.c
+++ super-trunfo.c
@@ -22,12 +22,27 @@ int main (){
     float km22 = 693.442;
 
     float PIB_ ;
-    float PIB1 = 27164.45;
-    float PIB2 = 21706.06;
+    float PIB1 = 65973702097.10;
+    float PIB2 = 58478263728.48;
     
     int Turismo_ ;
     int Turismo1 = 15;
     int Turismo2 = 23;
+
+    unsigned long int densidade_ = populacao_ / km2_;
+    unsigned long int densidade1 = populacao1 / km21;
+    unsigned long int densidade2 = populacao2 / km22;
+
+    unsigned long int ppc_ = PIB1 / populacao_;
+    unsigned long int ppc1 = PIB1 / populacao1;
+    unsigned long int ppc2 = PIB2 / populacao2;
+
+    float SuperPoder_ = populacao_ + km2_ + PIB_ + Turismo_ + densidade_ + ppc_; 
+    float SuperPoder1 = populacao1 + km21 + PIB1 + Turismo1 + densidade1 + ppc1; 
+    float SuperPoder2 = populacao2 + km22 + PIB2 + Turismo2 + densidade2 + ppc2; 
+
+    float resultadoDensidade, resultadoPPC;
+
 
     
     
@@ -44,7 +59,11 @@ int main (){
     printf ("AREA(km²): %.3f\n", km21);
     printf ("PIB(R$): %.2f\n", PIB1);
     printf ("PONTOS TURISTICOS: %d\n", Turismo1);
-    
+    printf ("DENSIDADE POPULACIONAL (hab/km²): %ld\n", densidade1);
+    printf ("PIB per capita: %ld\n", ppc1);
+    printf ("SUPER PODER: %.2f \n", SuperPoder1);
+
+
     printf ("_________________________________\n");
     
 
@@ -56,6 +75,22 @@ int main (){
     printf ("AREA(km²): %.3f\n", km22);
     printf ("PIB(R$): %.2f\n", PIB2);
     printf ("PONTOS TURISTICOS: %d\n", Turismo2);
+    printf ("DENSIDADE POPULACIONAL (hab/km²): %ld\n", densidade2);
+    printf ("PIB per capita: %ld\n", ppc2);
+    printf ("SUPER PODER: %.2f \n", SuperPoder2);
+
+    printf("-----------------------------------------------------\n");
+    
+
+    printf ("POPULACAO: %d \n",populacao1 > populacao2);
+    printf ("AREA (KM²) %d\n", km21 > km22);
+    printf ("PIB (R$): %d \n", PIB1 > PIB2);
+    printf ("PONTOS TURISTICOS:%d \n",Turismo1 > Turismo2);
+    printf ("DENSIDADE POPULACIONAL:%d \n",densidade1 < densidade2);
+    printf ("PIB per CAPITA:%d \n", ppc1 > ppc2);
+    printf ("Super Poder: %d\n", SuperPoder1 > SuperPoder2);
+    
+    
     printf("-----------------------------------------------------\n");
 
     printf ("MONTE SUA CARTA COM OS DADOS QUE FOREM PREENCHIDOS. \n");
@@ -88,6 +123,9 @@ int main (){
     scanf  ("%d", &Turismo_);
     printf ("a quanidade de pontos sao: %d\n", Turismo_);
 
+    resultadoDensidade = (float) populacao_ / km2_;
+    resultadoPPC = (float) PIB_ / populacao_;
+
     printf ("CARTA\n");
     printf ("ESTADO: %c\n", estado_);
     printf ("CODIGO: %s\n", codigo_);
@@ -96,5 +134,7 @@ int main (){
     printf ("AREA(km²): %.3f\n", km2_);
     printf ("PIB(R$): %.2f\n", PIB_);
     printf ("PONTOS TURISTICOS: %d\n", Turismo_);
+    printf ("DENSIDADE POPULACIONAL (hab/km²): %.2f\n", resultadoDensidade);
+    printf ("PIB per capita: %.2f\n",resultadoPPC);
     return 0;
 }
